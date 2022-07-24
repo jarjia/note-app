b6a9076ed84d6e4b0edfcb2951a2a4326c594422
jarji <100996174+jarjia@users.noreply.github.com>
2022-07-24 20:48:12 +0400

    updated

--- src/App.js
+++ src/App.js
@@ -14,17 +14,20 @@ const App = () => {
     ])
     const [textarea, setTextarea] = useState('')
     const [search, setSearch] = useState('')
+    const [darkMode, setDarkMode] = useState(false)
+    let letters = 'qwertyuiopasdfghjklzxcvbnm'
 
-    useEffect(()=>{
+    useEffect(() => {
+        document.body.style.background = darkMode ? '#111111' : 'white'
         const savedNotes = JSON.parse(localStorage.getItem('react-notes-app-data'))
         if(savedNotes){
           setNote(savedNotes)
         }
-      },[])
+    }, [darkMode])
 
-      useEffect(()=>{
+    useEffect(()=>{
         localStorage.setItem('react-notes-app-data',JSON.stringify(note))
-      },[note])
+    },[note])
     
     const handleSave = () => {
         setNote(prev => {
@@ -48,24 +51,30 @@ const App = () => {
         })
     }
 
+    const handleToggle = () => {
+        setDarkMode(prev => !prev)
+    }
+
   return (
-    <div className='wrapper'>
-        <Header setSearch={setSearch} search={search}/>
-        <div className='notes-wrapper'>
-            {note.filter(item => {
-                if(search === '') {
-                    return item
-                }else if(item.text.toLowerCase().includes(search.toLowerCase())) {
-                    return item
-                }
-            }).map(item => {
-                return <Note item={item} key={item.id} handleDelete={handleDelete} search={search}/>
-            })}
-            <div className='note-input'>
-                <textarea cols="10" rows="8" maxLength='300' value={textarea} onChange={e => setTextarea(e.target.value)} placeholder="Type to add a new note..."></textarea>
-                <div className='root-inp'>
-                    <span className='remaining'>{300 - textarea.length} Remaining</span>
-                    <button className='save' onClick={handleSave}>Save</button>
+    <div>
+        <div className='wrapper'>
+            <Header setSearch={setSearch} search={search} handleToggle={handleToggle} dark={darkMode}/>
+            <div className='notes-wrapper'>
+                {note.filter(item => {
+                    if(search === '') {
+                        return item
+                    }else if(item.text.toLowerCase().includes(search.toLowerCase())) {
+                        return item
+                    }
+                }).map(item => {
+                    return <Note item={item} key={item.id} handleDelete={handleDelete} search={search}/>
+                })}
+                <div className='note-input'>
+                    <textarea cols="10" rows="8" maxLength='300' value={textarea} onChange={e => setTextarea(e.target.value)} placeholder="Type to add a new note..." autoFocus></textarea>
+                    <div className='root-inp'>
+                        <span className='remaining'>{300 - textarea.length} Remaining</span>
+                        <button className='save' onClick={textarea.match(/[a-zA-Z0-9!@#$%^&*)(+=._-]/) === null ? undefined : handleSave}>Save</button>
+                    </div>
                 </div>
             </div>
         </div>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import './header.css'
 
-const Header = ({setSearch, search}) => {
+const Header = ({setSearch, search, handleToggle, dark}) => {
   console.log(search);
   return (
     <header>
-        <h1 className='header-h1'><span>React</span> Notes</h1>
+        <div style={{display: 'flex', justifyContent: 'space-between'}}>
+          <h1 className='header-h1' style={{color: dark ? 'white' : 'black'}}><span>React</span> Notes</h1>
+          <button className='btn' onClick={handleToggle}>Toggle Mode</button>
+        </div>
         <div className='input-div'>
             <input type='text' placeholder='Search for your notes' value={search} onChange={e => setSearch(e.target.value)}/>
         </div>
